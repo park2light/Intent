510df28b7121cee936b3b51453da40c509e31a70
박성현 <park2light@gmail.com>
2020-08-23 16:07:46 +0900

    메인화면과 메시지화면간의 데이터 전달 준비

---
+++ app/src/main/java/kr/co/tjoeun/intent/MessageActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
