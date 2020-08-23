60b5e44c9e7eefb46110833428f9f47cea244ae1
박성현 <park2light@gmail.com>
2020-08-23 15:54:02 +0900

    메인화면에서 다른화면으로 이동

--- app/src/main/java/kr/co/tjoeun/intent/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent/MainActivity.kt
@@ -1,11 +1,20 @@
 package kr.co.tjoeun.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        다른화면으로 이동하기 버튼이 눌리면 그때 넘어가게 한다.
+        moveToOtherActivityBtn.setOnClickListener {
+            val myIntent = Intent(this,OtherActivity::class.java) //티켓발권
+            startActivity(myIntent)
+        }
+
     }
 }
