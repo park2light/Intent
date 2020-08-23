c9fae64d0a6fe5016915b747917bcc1b086fb17f
박성현 <park2light@gmail.com>
2020-08-23 16:03:51 +0900

    다른화면에서 복귀버튼 누르면 => fisish() 처리로 화면종료(자동 복귀)

--- app/src/main/java/kr/co/tjoeun/intent/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 //        다른화면으로 이동하기 버튼이 눌리면 그때 넘어가게 한다.
         moveToOtherActivityBtn.setOnClickListener {
             val myIntent = Intent(this,OtherActivity::class.java) //티켓발권
-            startActivity(myIntent)
+            startActivity(myIntent) //티켓을 들고 실제 비행기에 탑승
         }
 
     }
--- app/src/main/java/kr/co/tjoeun/intent/OtherActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent/OtherActivity.kt
@@ -1,11 +1,21 @@
 package kr.co.tjoeun.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+
+        returnToMainBtn.setOnClickListener {
+
+            //val returnStart = Intent(this,MainActivity::class.java)
+            //startActivity(returnStart)
+            finish()
+        }
     }
 }
